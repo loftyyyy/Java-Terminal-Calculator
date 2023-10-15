9cd9a6d944bd75ec5c700c6a58f0e027b5041a4b
loftyyyy <lofty08.business@gmail.com>
2023-10-15 16:13:33 +0800

    Slight changes: added "Kcal" in every output

--- src/MET.java
+++ src/MET.java
@@ -42,7 +42,7 @@ public class MET {
                     //level*3.5*53/200 
                     for (int i = 0; i < metListIndex; i++){
                         if (metList60.size() > i) {
-                            System.out.println("MET value for 60 and 15 mins in level " + levelList.get(i) +  " are: " + metList60.get(i) + " and " + metList15.get(i));
+                            System.out.println("MET value for 60 and 15 mins in level " + levelList.get(i) +  " are: " + metList60.get(i) + " and " + metList15.get(i) + " Kcal");
                             System.out.println("\n");
                         }
                     }
